aea38e3178fa7d31125308b581d7b35b17c40fb0
Lahcen EDAIF <dev.lah.eda@gmail.com>
2025-09-20 13:27:51 +0200

    1- Add Base62 Encoding Test 1- Add Encoding Base62 Extention methodes

---
+++ Src/Core/Extentions/Base62Extentions.cs
@@ -0,0 +1,29 @@
+using System.Text;
+namespace Core.Extentions;
+public static class Base62Extentions
+{
+    const string _base62Digits = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public static string EncodeToBase62(this long number)
+    {
+        StringBuilder base62 = new();
+        while (number > 0)
+        {
+            base62.Append(_base62Digits[(int)number % 62]);
+            number /= 62;
+        }
+        return base62.ToString();
+    }
+    public static long DecodeFromBase62String(this string base62)
+    {
+        if (base62 is null || base62.Length == 0)
+            throw new ArgumentException("Argument should not be null or empty");
+        long result = 0;
+        int power = 0;
+        foreach (var character in base62.Reverse())
+        {
+            result += (long)(_base62Digits.IndexOf(character) * Math.Pow(62, power));
+            power++;          
+        }
+        return result;
+    }
+}
---
+++ Tests/Core/Base62EncodingScenarios.cs
@@ -0,0 +1,38 @@
+using Core.Extentions;
+
+namespace Core.Tests;
+
+public class Base62EncodingScenarios
+{
+    [Theory]
+    [InlineData(1, "1")]
+    [InlineData(197874136, "dog6k")]
+    [InlineData(21286525, "1rjB3")]
+    [InlineData(156654, "EKG")]
+    [InlineData(545361, "2hS9")]
+    [InlineData(19867612, "1lmtm")]
+    public void ShouldEncodeNumberToBase62(long number, string base62Encode)
+    {
+        Assert.Equal(number.EncodeToBase62(), base62Encode);
+    }
+
+
+    [Theory]
+    [InlineData("dog6k", 197874136)]
+    [InlineData("1rjB3", 21286525)]
+    [InlineData("EKG", 156654)]
+    [InlineData("2hS9", 545361)]
+    [InlineData("1lmtm", 19867612)]
+    public void ShouldDecodeBase32ToNumber(string base62, long number)
+    {
+        Assert.Equal(base62.DecodeFromBase62String(), number);
+    }
+
+
+    [Fact]
+    public void ShouldThrowExceptionIfPassedEmtyOrNullString()
+    {
+        Assert.Throws<ArgumentException>(()=>"".DecodeFromBase62String());
+    }
+
+}

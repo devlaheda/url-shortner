e75408b29bc71724ca1a61102ca9c9b723d8e53c
Lahcen EDAIF <dev.lah.eda@gmail.com>
2025-09-20 16:20:41 +0200

    Add Basic Test for URL generation skeleton

---
+++ Tests/Core/ShortUrlGenerationScenarios.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace Core.Tests;
+
+public class ShortUrlGenerationScenarios
+{
+    [Fact]
+    public void ShouldReturnShorlUrlForZero()
+    {
+        TokenProvider tokenProvider = new();
+        tokenProvider.AssignRange(0, 10);
+        ShortUrlGenerator shortUrlGenerator = new(tokenProvider);
+        string shortUrl = shortUrlGenerator.GenerateUniqueUrl();
+        Assert.Equal("0", shortUrl);
+
+    }
+    [Fact]
+    public void ShouldReturnShortUrlFor8566857()
+    {
+        TokenProvider tokenProvider = new();
+        tokenProvider.AssignRange(8566857, 1566857);
+        ShortUrlGenerator shortUrlGenerator = new(tokenProvider);
+        string shortUrl = shortUrlGenerator.GenerateUniqueUrl();
+        Assert.Equal("zWD7", shortUrl);
+    }
+}

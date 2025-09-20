493813f519b9377b710a227849f77afa1e06782a
Lahcen EDAIF <dev.lah.eda@gmail.com>
2025-09-20 17:00:35 +0200

    Fix Issue in ShortUrl Tests

--- Tests/Core/ShortUrlGenerationScenarios.cs
+++ Tests/Core/ShortUrlGenerationScenarios.cs
@@ -17,7 +17,7 @@ public class ShortUrlGenerationScenarios
     public void ShouldReturnShortUrlFor8566857()
     {
         TokenProvider tokenProvider = new();
-        tokenProvider.AssignRange(8566857, 1566857);
+        tokenProvider.AssignRange(8566857, 10566857);
         ShortUrlGenerator shortUrlGenerator = new(tokenProvider);
         string shortUrl = shortUrlGenerator.GenerateUniqueUrl();
         Assert.Equal("zWD7", shortUrl);

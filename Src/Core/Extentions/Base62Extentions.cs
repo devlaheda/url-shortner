9730062aa423a8d5461267649c1663f4d466664b
Lahcen EDAIF <dev.lah.eda@gmail.com>
2025-09-20 16:19:35 +0200

    Fix Base62 Implimentation

--- Src/Core/Extentions/Base62Extentions.cs
+++ Src/Core/Extentions/Base62Extentions.cs
@@ -5,13 +5,16 @@ public static class Base62Extentions
     const string _base62Digits = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     public static string EncodeToBase62(this long number)
     {
-        StringBuilder base62 = new();
+        if (number  == 0)
+            return "0";
+        Stack<char> base62 = new();
+        
         while (number > 0)
         {
-            base62.Append(_base62Digits[(int)number % 62]);
+            base62.Push(_base62Digits[(int)number % 62]);
             number /= 62;
         }
-        return base62.ToString();
+        return new string(base62.ToArray());
     }
     public static long DecodeFromBase62String(this string base62)
     {
